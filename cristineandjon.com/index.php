349b0070c1cdb7eed7996d93d1830e26b919c6cf
jonseitz <jonseitz@p3plcpnl0161.prod.phx3.secureserver.net>
2014-11-18 19:43:10 -0700

    first commit

---
+++ cristineandjon.com/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cristine and Jonathan Are Getting Married</title>
+	   <link rel="stylesheet" type="text/css" href="style.css">
+	   <meta charset="UTF-8" />
+	   <meta name="viewport" content="width=device-width, user-scalable=no">
+	   <script type="text/javascript" src="//use.typekit.net/oks8aum.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<link rel="shortcut icon" href="favicon.ico" />
+	</head>
+	<body>
+		<div class="translucent">
+			<header>
+				<img id="logo" src="images/CristineJonLogo.png" alt="Cristine Bertochi and Jonathan Seitz are getting married!" title="Cristine Bertochi and Jonathan Seitz are getting married!"/>
+				<h1>Saturday, November 1st 2014</h1>
+				<h1>Four o' clock</h1>
+				<h1>Groveland, Massachusetts</h1>
+			</header>
+			<article>
+				<h2>Ceremony and Reception</h2>
+					<p><strong>Groveland Fairways</strong><br />156 Main Street<br />Groveland, MA 01834</p>
+					<p>The venue has plenty of parking on-site. There will also be a shuttle bus to bring guests from the hotel to Groveland Fairways and back. It will leave the hotel at 3 and 3:20 p.m., and return after the reception ends at 9:30 p.m.</strong>
+				<h2>The Wedding Party</h2>
+					<p>We will be joined by some of our closest friends and family:</p>
+					<div class="party-block">
+						<div class="party-right">
+							<h3>With the Bride:</h3>
+							<p><strong>Allyson Coughlin</strong></p>
+							<p><strong>Katie O'Brien</strong></p>
+							<p><strong>Anna Seitz</strong></p>
+						</div>
+						<div class="party-left">
+							<h3>With the Groom:</h3>
+							<p><strong>David Seitz</strong></p>
+							<p><strong>Aaron Seitz</strong></p>
+							<p><strong>Mike Bertochi</strong></p>
+							<p><strong>Kevin Sirois</strong></p>
+							<p><strong>Zach Stone</strong></p>
+						</div>
+					</div>
+				<h2>Hotel Information</h2>
+					<p><strong>The Fairfield Inn</strong><br />35 Clarks Road<br />Amesbury, MA 01913<br /></p>
+					<p>We've reserved a block of rooms for the weekend. You can <a href="http://cwp.marriott.com/mhtfa/seitz2014/">book your room online</a>, or call the hotel at 978-388-3400. For phone reservations, the group code is BERS.</p>
+				<h2>Map</h2>
+					<div class="map">
+						<iframe src="https://mapsengine.google.com/map/embed?mid=zLhqdBcIyRk8.krQx_4ubDbg4" width="100%" height="400"></iframe>
+					</div>
+				<h2>Other Points of Interest</h2>
+					<p>For guests who will be spending some extra time in town, there is plenty to do in Boston and the North Shore:</p>
+					<p>The wedding will be held on Halloween weekend, so there will be <a href="http://hauntedhappenings.org/">lots going on</a> in <strong>Salem</strong>. For those arriving on Friday night who would prefer to avoid the accompanying traffic, we recommend taking I-93 North from Logan Airport and taking I-495 North to Amesbury.</p>
+					<p>The neighboring town of <strong>Newburyport</strong> has a number of restaurants and boutiques, as well as a beautiful view of the Merrimack River.</p> 
+					<p>First-time visitors venturing into <strong>Boston</strong> should see Boston Common and the Public Gardens, or walk down Newbury Street in the city's Back Bay neighborhood.</p>
+					<p>If you'd you like to stop off in <strong>Somerville</strong>, you can also visit Prospect Hill Park, where the first American flag was raised during the Revolutionary War. It's also the place where Jon proposed, and where Cristine said yes.</p>					
+				<h2>Questions or Correspondence?</h2>
+					<p>You can reach us at <a href="mailto:contact@cristineandjon">contact@cristineandjon.com</a></p>
+					<p class="credits">Illustration by <a href="http://hulloart.tumblr.com/">Ross Jackson</a></p>
+			</article>
+		</div>
+	</body>
+</html>
